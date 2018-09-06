3cdcf3e9ab00cb19b2c028058a5a047681f4e39b
jancy-joseph <jancyworkmail@gmail.com>
2018-09-06 09:52:39 -0700

    commit3

--- MyMusic/MusicFile.cs
+++ MyMusic/MusicFile.cs
@@ -4,11 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace MyMusic
 {
     class MusicFile
     {
+        /*
+        public string Album { get; set; }
+        public string AlbumArtist { get; set; }
+        public string Artist { get; set; }
+        public uint Bitrate { get; }
+      //  public IList<string> Composers { get; }
+      //  public IList<string> Conductors { get; }
+        public TimeSpan Duration { get; }
+        public IList<string> Genre { get; }
+       // public IList<string> Producers { get; }
+       // public string Publisher { get; set; }
+        //public uint Rating { get; set; }
+        public string Subtitle { get; set; }
+        public string Title { get; set; }
+        public uint TrackNumber { get; set; }
+       // public IList<string> Writers { get; }
+        public uint Year { get; set; }
+
+    */
+
+        public StorageFile MFile { get; set; }
+        public string MFileName { get; set; }
+        public string McoverImage { get; set; } = "Assets/cd-654603_1280.png";
+        public string MAlbum { get; set; }
+        public string MArtist { get; set; }
+        public string MTitle { get; set; }
 
     }
 }
--- MyMusic/MyMusicCollection.xaml.cs
+++ MyMusic/MyMusicCollection.xaml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +16,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Pickers;
+using System.Diagnostics;
+using System.Text;
+using Windows.Media.Playlists;
+using Windows.Storage.FileProperties;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,24 +31,75 @@ namespace MyMusic
     /// </summary>
     public sealed partial class MyMusicCollection : Page
     {
+        MediaPlayer MyPlayer;
+        StorageFile file;
+
         public MyMusicCollection()
         {
             this.InitializeComponent();
+            MyPlayer = new MediaPlayer();
+            LoadMyFullCollection();
+
+
         }
-        public static LoadMyFullCollection{
-            //
+        public async void LoadMyFullCollection()
+        {
+            var MusicFileList = new List<MusicFile>();
+            //var jObj = new JsonObject();
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.MusicLibrary;
+
+            picker.FileTypeFilter.Add(".mp3");
+            picker.FileTypeFilter.Add(".wav");
+            picker.FileTypeFilter.Add(".wma");
+            picker.FileTypeFilter.Add(".ogg");
+
+            IReadOnlyList<StorageFile> FilesDiscovered = await picker.PickMultipleFilesAsync();
+            
+            if (FilesDiscovered.Count > 0)
+            {
+                //StringBuilder output = new StringBuilder("Picked files:\n");
+
+                // Application now has read/write access to the picked file(s)
+                foreach (Windows.Storage.StorageFile fileToAdd in FilesDiscovered)
+                {
+                    MusicProperties musicProperties = await fileToAdd.Properties.GetMusicPropertiesAsync();
+                    //MyPlayer.AutoPlay = false;
+                    //MyPlayer.Source = MediaSource.CreateFromStorageFile(fileToAdd);
+
+                    var mymusic = new MusicFile()
+                    {
+                        MFileName = fileToAdd.Name,
+                        MFile = fileToAdd,
+                        MAlbum = musicProperties.Album,
+                        MArtist = musicProperties.Artist,
+                        MTitle= musicProperties.Title
+
+                    };
+                    MusicFileList.Add(mymusic);
+                   //this.ChoosePlaylist1.Items.Add(fileToAdd.Path);
+                    // output.Append(file.Name + "\n");
+                }
+                //  this.textBlock.Text = output.ToString();
+            }.
+            else
+            {
+                //this.ChoosePlaylist1.Items.Add("Operation cancelled.");
             }
-    public static AddMusicToFullCollectiontocheckbox()
-    {
+
         }
-     public static RemoveMusicFromColelction()
-    {
+        public static void AddMusicToFullCollectiontocheckbox()
+        {
+        }
+        public static void RemoveMusicFromColelction()
+        {
 
-    }
-     public static playMusic()
-    {
+        }
+        public static void playMusic()
+        {
 
-    }
+        }
 
 
     }
